9514a3416e1710f18f6b16590ca12f6df83bcc2f
Nikketryhard <louisnewmobile@gmail.com>
2026-03-05 06:43:59 -0600

    feat: RebaseTracker.hours_until_next

--- hydra-train/src/training/drda.rs
+++ hydra-train/src/training/drda.rs
@@ -43,6 +43,10 @@ impl RebaseTracker {
         }
     }
 
+    pub fn hours_until_next(&self) -> f32 {
+        (self.rebase_interval_hours - self.gpu_hours_since_rebase).max(0.0)
+    }
+
     pub fn is_overdue(&self, factor: f32) -> bool {
         self.gpu_hours_since_rebase >= self.rebase_interval_hours * factor
     }
